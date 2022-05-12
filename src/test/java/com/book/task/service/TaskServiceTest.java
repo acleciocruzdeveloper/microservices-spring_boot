7613dd47615f23a5cc55e8a2b7b18675c54b8269
aclecio <aclecioscruz@gmail.com>
2022-05-12 11:19:19 -0300

    Atualização de dependências, implementação dos  Mocks e classe TaskServiceTest

---
+++ src/test/java/com/book/task/service/TaskServiceTest.java
@@ -0,0 +1,93 @@
+package com.book.task.service;
+
+
+import com.book.task.TaskMock;
+import com.book.task.entity.Task;
+import com.book.task.repositories.TaskRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import static org.junit.Assert.*;
+
+@ExtendWith(MockitoExtension.class)
+@RunWith(JUnitPlatform.class)
+public class TaskServiceTest {
+
+    @Mock
+    TaskRepository taskRepository;
+    private TaskService taskService;
+
+    @BeforeEach
+    public void setup() {
+        taskService = new TaskService(taskRepository);
+
+        Pageable pageable = PageRequest.of(0, 5, Sort.by(
+                Sort.Order.asc("name"),
+                Sort.Order.desc("id")));
+        Mockito.lenient().when(taskRepository.findAll(pageable)).thenReturn(TaskMock.createTasks());
+
+    }
+
+    @Test
+    @DisplayName("Should return all tasks")
+    public void getTasks_happypath() {
+        Pageable pageable = PageRequest.of(0, 5, Sort.by(
+                Sort.Order.asc("name"),
+                Sort.Order.desc("id")));
+
+        Page<Task> tasks = taskService.getTasks(pageable);
+
+        assertEquals(tasks.getTotalPages(), 1);
+        assertEquals(tasks.getTotalElements(), 2);
+        assertNotNull(tasks);
+    }
+
+    @Nested
+    @DisplayName("Happy Tests")
+    class happycases {
+
+        @Test
+        void justtest() {
+            String name = "just testing";
+            assertEquals(name, "just testing");
+
+        }
+
+        @Test
+        void justtest1() {
+            String name = "just testin";
+            assertEquals(name, "just testin");
+
+        }
+    }
+
+    @Nested
+    @DisplayName("Unhappy Test")
+    class unhappycases {
+
+        @Test
+        void justtest(){
+            String name = "just testing";
+            assertEquals(name, "just testing");
+
+        }
+        @Test
+        void justtest1(){
+            String name = "just testing";
+            assertNotEquals(name, "just testincc");
+        }
+    }
+
+}
